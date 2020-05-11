891123b06b59b6fc0407ca143eeae5b43c7d1abc
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-11 13:10:41 +0530

    add reco

---
+++ fingcppyrecog.py
@@ -0,0 +1,24 @@
+import boto3
+rekognition = boto3.client("rekognition")
+
+def handler(request):
+    try:
+        data = rekognition.compare_faces(
+            SourceImage={
+                'S3Object': {
+                    'Bucket': "cloud9-ktest",
+                    'Name': "AS2station.png"
+                }
+            },
+            TargetImage={
+                'S3Object': {
+                    'Bucket': "cloud9-ktest",
+                    'Name': "shot-buffalo-5daeeb9351a540214a19d115-wiu.png"
+                }
+            }
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
+    
+    return "Successfully executed"
